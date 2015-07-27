7181ce49bed440986fb60b76802306ae1bf51da1
git repository hosting <gitolite3@flodsvin>
2015-07-27 13:47:32 +0200

    Now working

--- backup.sh
+++ backup.sh
@@ -3,11 +3,10 @@
 rsync -a -v -u /var/lib/gitolite3/ /var/lib/backup-gitolite/gitolite3
 
 
-pushd /var/lib/backup-gitolite/gitolite3
+cd /var/lib/backup-gitolite/gitolite3
 
 git add *
 git add */.*
 git commit -m "Regular commit"
 
 git push origin master
-popd
